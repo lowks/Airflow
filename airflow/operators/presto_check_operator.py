030983f46f0be6ef2d9873d708e0dae2b8ccf81d
Arthur Wiedmer <arthur.wiedmer@gmail.com>
2015-01-07 23:15:41 +0000

    [presto_check] Added draft presto_check operator
    This is similar to a sensor operator and checks non-empty/zero
    results to a SQL query run in the presto_client

---
+++ airflow/operators/presto_check_operator.py
@@ -0,0 +1,45 @@
+import logging
+
+from airflow.configuration import conf
+from airflow.hooks.presto import PrestoHook
+from airflow.models import BaseOperator
+from airflow.utils import apply_defaults
+
+
+class PrestoCheckOperator(BaseOperator):
+    """
+    Performs a simple check using sql code in a specific Presto database.
+
+    :param sql: the sql to be executed
+    :type sql: string
+    :param presto_dbid: reference to the Hive database
+    :type presto_dbid: string
+    """
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'PrestoOperator'
+    }
+    template_fields = ('sql',)
+    template_ext = ('.hql', '.sql',)
+
+    @apply_defaults
+    def __init__(
+            self, sql,
+            presto_dbid=conf.get('hooks', 'PRESTO_DEFAULT_DBID'),
+            *args, **kwargs):
+        super(PrestoCheckOperator, self).__init__(*args, **kwargs)
+
+        self.presto_dbid = presto_dbid
+        self.hook = PrestoHook(presto_dbid=presto_dbid)
+        self.sql = sql
+
+    def check(self):
+        logging.info('Executing SQL check: ' + self.sql)
+        records = self.hook.get_records(hql=self.sql)
+        if not records:
+            return False
+        else:
+            if str(records[0][0]) in ('0', '',):
+                return False
+            else:
+                return True
